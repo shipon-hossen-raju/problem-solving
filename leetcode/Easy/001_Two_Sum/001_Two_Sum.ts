348f6606ba9019eb0cd6b6676b53e05a38e9fff3
Shipon Hossen Raju <msshipon234@gmail.com>
2025-10-17 23:25:40 +0600

    leetcode problem 1 solved

---
+++ leetcode/Easy/001_Two_Sum/001_Two_Sum.ts
@@ -0,0 +1,43 @@
+export {};
+
+/*
+🔢 Problem: 1. Two Sum
+🔗 Link: https://leetcode.com/problems/two-sum
+📘 Difficulty: Easy
+
+ 📝 description:
+      Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+      You may assume that each input would have exactly one solution, and you may not use the same element twice.
+      You can return the answer in any order.
+
+ 📌 Example:
+   Example 1:
+      Input: nums = [2,7,11,15], target = 9
+      Output: [0,1]
+      Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+   Example 2:
+      Input: nums = [3,2,4], target = 6
+      Output: [1,2]
+   
+   Example 3:
+      Input: nums = [3,3], target = 6
+      Output: [0,1]
+
+*/
+
+function twoSum(nums: number[], target: number) {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        return [i, j];
+      }
+    }
+  }
+  return [];
+}
+
+const nums = [2, 7, 11, 15];
+const target = 9;
+const result = twoSum(nums, target);
+console.log(result);
